23879ca938f4412c732732b5ebbef46724d8e425
Noone <kasraam385@gmail.com>
2025-07-31 19:10:09 +0430

    auth.js added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-import Image from "next/image";
+import { auth, signIn, signOut } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
-    <div>hellow World</div>
+    <div>
+      <form
+        action={async () => {
+          "use server";
+          if (session) {
+            await signOut();
+          } else {
+            await signIn("google");
+          }
+        }}
+      >
+        {session ? (
+          <>
+            <p>سلام، {session.user?.name}</p>
+            <button type="submit">Sign out</button>
+          </>
+        ) : (
+          <button type="submit">Sign in with Google</button>
+        )}
+      </form>
+    </div>
   );
 }
