d5ab9a5e8c8c9789159d42d46cc62d7ee8450863
Noone <kasraam385@gmail.com>
2025-08-02 11:49:43 +0430

    connect auth to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,30 @@ export default async function Home() {
 
   return (
     <div>
-      <form
-        action={async () => {
-          "use server";
-          if (session) {
-            await signOut();
-          } else {
-            await signIn("google");
-          }
-        }}
-      >
-        {session ? (
-          <>
-            <p>سلام، {session.user?.name}</p>
-            <button type="submit">Sign out</button>
-          </>
-        ) : (
-          <button type="submit">Sign in with Google</button>
-        )}
-      </form>
+      <div className="grid grid-cols-12 mx-7">
+        <form
+          className="bg-red-400 col-span-3"
+          action={async () => {
+            "use server";
+            if (session) {
+              await signOut();
+            } else {
+              await signIn("google");
+            }
+          }}
+        >
+          {session ? (
+            <>
+              <p>سلام، {session?.user?.name}</p>
+              <button type="submit">Sign out</button>
+            </>
+          ) : (
+            <button type="submit">Sign in with Google</button>
+          )}
+        </form>
+        <div className="col-span-6">s</div>
+        <div className="col-span-3">s</div>
+      </div>
     </div>
   );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,38 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import dbConnect from "./lib/db";
+import User from "./models/user";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await dbConnect();
+
+        const existingUser = await User.findOne({ email: user.email });
+
+        if (!existingUser) {
+          await User.create({
+            email: user.email,
+            name: user.name,
+          });
+          console.log(`User Created!`);
+        } else {
+          console.log("User Already Exist");
+        }
+
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+  },
   secret: process.env.AUTH_SECRET,
-})
+});
