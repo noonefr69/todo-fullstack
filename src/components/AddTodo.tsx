c767988d55110b169fb4e011f2e484b1e8561c96
Noone <kasraam385@gmail.com>
2025-08-05 18:13:31 +0430

    changinf format little bit of Post method add post component

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import { handlePost } from "@/actions/hadleTodoActions";
-import { auth } from "@/auth";
-import { Plus } from "lucide-react";
-import React from "react";
+import { Loader, Plus } from "lucide-react";
+import { useTransition } from "react";
 
-export default async function AddTodo() {
-  const session = await auth();
+export default function AddTodo() {
+  const [isPending, startTransition] = useTransition();
 
-  return session?.user ? (
+  function handleChange(formData: FormData) {
+    startTransition(() => {
+      handlePost(formData);
+    });
+  }
+
+  return (
     <div className="py-14 shadow-sm dark:shadow-amber-50 rounded-lg mb-7 ">
-      <form action={handlePost} className="flex px-5 gap-5 items-center">
+      <form action={handleChange} className="flex px-5 gap-5 items-center">
         <input
           className="w-full outline-none border-b-2 border-r-2 py-3 px-5 text-lg shadow-sm dark:shadow-amber-50 duration-300 focus:border-r-[#00000094] focus:border-b-[#00000094] dark:focus:border-r-amber-50 dark:focus:border-b-amber-50"
           type="text"
@@ -17,12 +24,14 @@ export default async function AddTodo() {
           required
           placeholder="Add Todo"
         />
-        <button type="submit" className="cursor-pointer">
-          <Plus size={35} />
-        </button>
+        {isPending ? (
+          <Loader className="animate-spin" size={35}/>
+        ) : (
+          <button type="submit" className="cursor-pointer">
+            <Plus size={35} />
+          </button>
+        )}
       </form>
     </div>
-  ) : (
-    ""
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -16,29 +16,31 @@ export default async function Todos() {
   console.log(todos);
 
   return (
-    <div className="px-5 py-7 shadow-sm min-h-[calc(100vh-37vh)] dark:shadow-amber-50 rounded-lg">
+    <div className="px-3 py-7 shadow-sm min-h-[calc(100vh-37vh)] dark:shadow-amber-50 rounded-lg">
       <ul className="space-y-1">
         {todos.map((todo) => {
           return (
             <li
-              className="flex items-center justify-between break-all gap-5"
+              className="flex items-center justify-between break-all gap-5 duration-300 hover:bg-slate-100 dark:hover:bg-[rgb(36,36,36)] rounded-md px-2 py-1"
               key={todo._id.toString()}
             >
               <div className="flex items-center gap-2">
                 <Checked id={todo._id.toString()} checked={todo.completed} />
                 <span
                   className={`text-lg font-semibold ${
-                    todo.completed ? "line-through text-slate-500" : "text-slate-900"
+                    todo.completed
+                      ? "line-through text-slate-500 dark:text-slate-400"
+                      : "text-slate-800 dark:text-slate-100"
                   }`}
                 >
                   {todo.title}
                 </span>
               </div>
               <div className="flex items-center gap-2">
-                <Delete id={todo._id.toString()}/>
+                <Delete id={todo._id.toString()} />
                 <Edit
                   size={20}
-                  className="cursor-pointer text-slate-500 duration-300 hover:text-slate-800"
+                  className="cursor-pointer duration-300 text-slate-500 dark:text-slate-400 dark:hover:text-slate-50 hover:text-slate-800"
                 />
               </div>
             </li>
